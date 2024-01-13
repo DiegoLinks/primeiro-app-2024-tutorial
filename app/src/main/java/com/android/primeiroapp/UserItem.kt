70fdf84dc54fd6a3c750931718e2ecfb0e11b0b1
Diego <diego.rof@hotmail.com>
2024-01-13 16:11:43 -0300

    Preview da classe UserItem

--- app/src/main/java/com/android/primeiroapp/UserItem.kt
+++ app/src/main/java/com/android/primeiroapp/UserItem.kt
@@ -1,8 +1,23 @@
 package com.android.primeiroapp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun UserItem(user: User) {
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserItemPreview() {
+
+    val user = User(
+        name = "Jhon",
+        nickname = "@Jhon",
+        bio = "Uma descrição qualquer pra bio do Jhon.",
+        profileImg = ""
+    )
+
+    UserItem(user = user)
 }
