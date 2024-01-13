f5944f222ef230125da0a11c0ebfcd989453b665
Diego <diego.rof@hotmail.com>
2024-01-13 16:25:50 -0300

    Estrutura inicial do card do usuário

--- app/src/main/java/com/android/primeiroapp/UserItem.kt
+++ app/src/main/java/com/android/primeiroapp/UserItem.kt
@@ -1,11 +1,35 @@
 package com.android.primeiroapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun UserItem(user: User) {
+    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+        Text(
+            text = user.name,
+            modifier = Modifier.padding(top = 4.dp)
+        )
 
+        Text(
+            text = user.nickname,
+            modifier = Modifier.padding(top = 4.dp),
+            color = Color.Gray
+        )
+
+        Text(
+            text = user.bio,
+            modifier = Modifier.padding(top = 4.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
