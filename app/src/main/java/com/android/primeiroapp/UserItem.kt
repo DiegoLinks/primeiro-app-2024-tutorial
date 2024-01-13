032397ed0fee33db0218864df1dc86ab1e6e6661
Diego <diego.rof@hotmail.com>
2024-01-13 17:17:50 -0300

    Usuário clicável

--- app/src/main/java/com/android/primeiroapp/UserItem.kt
+++ app/src/main/java/com/android/primeiroapp/UserItem.kt
@@ -1,5 +1,7 @@
 package com.android.primeiroapp
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -19,10 +22,17 @@ import coil.compose.AsyncImage
 @Composable
 fun UserItem(user: User) {
 
+    val context = LocalContext.current
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
+            .clickable {
+                Toast
+                    .makeText(context, "Você seguiu " + user.name, Toast.LENGTH_SHORT)
+                    .show()
+            }
     ) {
 
         AsyncImage(
