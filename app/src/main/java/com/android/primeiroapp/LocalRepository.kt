1a5669aec4d0840036b5508837d4aa37e796260f
Diego <diego.rof@hotmail.com>
2024-01-13 16:43:49 -0300

    Classe repositório com os dados dos usuários

---
+++ app/src/main/java/com/android/primeiroapp/LocalRepository.kt
@@ -0,0 +1,76 @@
+package com.android.primeiroapp
+
+val userList = listOf(
+    User(
+        name = "Clara Nunes",
+        nickname = "@clara",
+        bio = "O amor será eterno novamente",
+        profileImg = "https://bhaz.com.br/wp-content/uploads/2023/01/clara-nunes.jpg"
+    ),
+    User(
+        name = "Thomas Shelby",
+        nickname = "@tShelby",
+        bio = "Frio e calculista...",
+        profileImg = "https://br.web.img2.acsta.net/newsv7/21/09/29/23/38/1439911.jpg"
+    ),
+    User(
+        name = "Agrafena Alexandrovna",
+        nickname = "@grushenka",
+        bio = "Você sabia que Kotlin também é russa?",
+        profileImg = "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/large/800/Grushenka.The-Brothers-Karamazov.webp"
+    ),
+    User(
+        name = "Diego",
+        nickname = "@DiegoLinks",
+        bio = "Android Engineer. I create content about Android development on my YouTube channel DiegoLinks.",
+        profileImg = "https://ichef.bbci.co.uk/news/640/cpsprodpb/2422/live/f89f7b30-2565-11ee-9cd6-895597f4f85e.jpg"
+    ),
+    User(
+        name = "Babi Loud",
+        nickname = "@loudB",
+        bio = "Life is like a game.",
+        profileImg = "https://i.superesportes.com.br/NI3g1X_iSysv_Zz6Lwmtm_iGY10=/0x800/smart/imgsapp.mg.superesportes.com.br/app/foto_126510467054/2021/01/03/12069/20230101223334934123i.jpg"
+    ),
+    User(
+        name = "Alberro S. Dumont",
+        nickname = "@dumont",
+        bio = "Acredito que posso voar!",
+        profileImg = "https://jpimg.com.br/uploads/2019/09/santos-dumont.jpg"
+    ),
+    User(
+        name = "Jet Brains",
+        nickname = "@JetBrains",
+        bio = "O Android Studio travou a máquina antes que eu pudesse terminar de",
+        profileImg = "https://avatars.githubusercontent.com/u/878437?s=200&v=4"
+    ),
+    User(
+        name = "Eduardo",
+        nickname = "@tostao",
+        bio = "Caminhando, escrevendo, codando...",
+        profileImg = "https://static-img.zz.pt/history/imgS620I11658T20190122154103.png"
+    ),
+    User(
+        name = "Liskov B.",
+        nickname = "@barbara_liskov",
+        bio = "ESTUDA SOLID GALERA!! na moralzinha",
+        profileImg = "https://cdn.britannica.com/86/129086-004-A93A7277/Barbara-Liskov-Turing-Award-2008.jpg"
+    ),
+    User(
+        name = "Joji",
+        nickname = "@PinkGuy",
+        bio = "Perfect don't mean that it's working. So what can I do? Cause sometimes I look in her eyes and that's where I find a glimpse of us. And I try to fall for her touch. But I'm thinkin' of the way it was said I'm fine and said I moved on. I'm only here passin' time in her arms, hopin' I find a glimpse of us",
+        profileImg = "https://akamai.sscdn.co/tb/letras-blog/wp-content/uploads/2022/07/4930a5e-Joji-1024x741.jpg"
+    ),
+    User(
+        name = "Florina Muntenescu",
+        nickname = "@florina-muntenescu",
+        bio = "",
+        profileImg = "https://avatars.githubusercontent.com/u/2998890?v=4"
+    ),
+    User(
+        name = "Alex Turner",
+        nickname = "@alexturner",
+        bio = "I wanna be your Ford Cortina I will never rust...",
+        profileImg = "https://collectionimages.npg.org.uk/large/mw263083/Alex-Turner.jpg"
+    ),
+)
